1fe9511832dd88863df4b00fa7d33be4bcacf655
hung <dthung1602@gmail.com>
2019-05-07 00:57:32 +0700

    Fix bug change password

--- src/main/java/vgu/group1/examregister/views/assistant/user/ChangePass.java
+++ src/main/java/vgu/group1/examregister/views/assistant/user/ChangePass.java
@@ -15,7 +15,7 @@ public class ChangePass extends BaseView {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Response doGet() throws IOException {
-        return Response.ok(getHTMLFile("/assistant/changepass.html")).build();
+        return Response.ok(getHTMLFile("assistant/changepass.html")).build();
     }
 
     @POST
